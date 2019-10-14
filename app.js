bd10bd239133a697f71798b42e0dda9242e51be6
Josh Yap <joshua.c.yap@gmail.com>
2019-10-13 23:53:34 -0500

    setting up async await fetch

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // https://api.chess.com/pub/player/yosh713
 // https://api.chess.com/pub/player/yosh713/stats
 // https://youtu.be/C4t6qfHZ6Tw?t=465
+// https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+// const url = 'https://api.chess.com/pub/player/yosh713/stats';
+
+// const response = await fetch('https://api.chess.com/pub/player/yosh713/stat');
+// const myJson = await response.json();
+// console.log(JSON.stringify(myJson));
+
+
+async function getData() {
+    const response = await fetch('https://api.chess.com/pub/player/yosh713/stats');
+    const myJson = await response.json();
+    console.log(myJson);
+    // document.getElementsByClassName('ratings').innerHTML = myJson;
+}
+
+getData();
+
 
 var data = [1, 2, 3, 4, 5];
 
