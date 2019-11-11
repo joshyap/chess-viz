e8312e9e2bcfa33bbc8554874917403ede241e4e
Josh Yap <joshua.c.yap@gmail.com>
2019-11-10 23:03:52 -0600

    rendering bar charts, removing excess code, cleaning up formatting

--- app.js
+++ app.js
@@ -1,71 +1,37 @@
-// https://api.chess.com/pub/player/yosh713
-// https://api.chess.com/pub/player/yosh713/stats
-// https://youtu.be/C4t6qfHZ6Tw?t=465
-// https://scotch.io/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
-//https://www.youtube.com/watch?v=uxf0--uiX0I
-
 const chess_url = 'https://api.chess.com/pub/player/yosh713/stats';
 
 async function getChessData() {
     const response = await fetch(chess_url);
     const data = await response.json();
-    console.log(data);    
 
-    document.getElementById('chess_blitz').textContent = data.chess_blitz.best.rating;
-    document.getElementById('chess_bullet').textContent = data.chess_bullet.best.rating;
-    document.getElementById('lessons').textContent = data.lessons.highest.rating;
-    document.getElementById('puzzle_rush').textContent = data.puzzle_rush.best.score;
-    document.getElementById('tactics').textContent = data.tactics.highest.rating;
-
+    // let chessData = [
+    //     String(data.chess_blitz.best.rating + " blitz "),
+    //     String(data.chess_bullet.best.rating + " bullet "),
+    //     String(data.lessons.highest.rating + " lessons "),
+    //     String(data.puzzle_rush.best.score + " puzzle rush "),
+    //     String(data.tactics.highest.rating + " tactics ")
+    // ]
 
     let chessData = [
         data.chess_blitz.best.rating,
         data.chess_bullet.best.rating,
         data.lessons.highest.rating,
-        data.puzzle_rush.best.score,
+        // data.puzzle_rush.best.score,
         data.tactics.highest.rating
     ]
+
+    console.log(chessData);
     
-    d3.select(".d3-ratings").selectAll("p")
+    d3.select(".d3_ratings").selectAll("p")
         .data(chessData)
         .enter()
         .append("p")
         .text((d) => d + " score");
 
-    console.log(chessData);
-    
-    for (var i = 0; i<chessData.length; i++) {
-        d3.select("ul").append("li").text(chessData[i])
-    };
+    d3.select(".d3_chart").selectAll("div")
+        .data(chessData)
+        .enter()
+        .append("div")
+        .attr("class", "bar")        
+        .style("height", (d) => d/5 + "px");                
 }
-
-
-
-// var data = ['d3 data will be loaded here'];
-// let data = [
-//     data.chess_blitz.best.rating,
-//     data.chess_bullet.best.rating,
-//     data.chess_blitz.best.rating,
-//     data.chess_blitz.best.rating,
-//     data.tactics.highest.rating
-// ]
-
-// console.log(data);
-
-// var x = d3.scale.linear()
-//     .domain([0, d3.max(data)])
-//     .range([0, 420]);
-
-// d3.select(".chart")
-//   .selectAll("div")
-//     .data(data)
-//   .enter().append("div")
-//     .style("width", function(d) { return x(d) + "px"; })
-//     .text(function(d) { return d; });
-
-// d3.select('.chart')
-//     .selectAll('p')
-//     .data(data)
-//     .enter()
-//     .append('p')
-//     .text(function(d) { return d; });
